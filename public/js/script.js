2e8566994b7117da3f464c662b521ee61143cc64
julianvazq <50368117+julianvazq@users.noreply.github.com>
2019-11-21 23:21:06 -0500

    Add background-image + logo / Style index.html

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,24 @@
 /* Code for initializting the map */
 /* code provided by the leaflet quickguide */
 /* coordinates here are for college park */
-let mymap = L.map('map').setView([38.987502, -76.942683], 10);
+let mymap = L.map("map").setView([38.987502, -76.942683], 10);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox.streets'
-}).addTo(mymap);
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+  {
+    maxZoom: 18,
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: "mapbox.streets"
+  }
+).addTo(mymap);
 /* testing some generic pop up and location */
 /* coordinates are set to UMD at the moment */
 var layer1 = L.layerGroup().addTo(mymap);
 let umd_mark = L.marker([38.987502, -76.942683]).addTo(mymap);
-umd_mark.bindPopup("The University of Maryland").openPopup();
+umd_mark.bindPopup("University of Maryland").openPopup();
 umd_mark.addTo(layer1);
 
 function loadSpeedCams(e) {
@@ -24,7 +28,8 @@ function loadSpeedCams(e) {
     .then(res => {
       console.log("Speed Cameras array: ", res); // logging step to check what we got
       return res;
-    }).then(res => mapSpeedData(res, true));
+    })
+    .then(res => mapSpeedData(res, true));
 }
 
 function loadRedLightCams(e) {
@@ -34,7 +39,8 @@ function loadRedLightCams(e) {
     .then(res => {
       console.log("Red Light Cameras array: ", res); // logging step to check what we got
       return res;
-    }) .then(res => mapRedLightData(res, true));
+    })
+    .then(res => mapRedLightData(res, true));
 }
 
 function loadBoth(e) {
@@ -43,16 +49,16 @@ function loadBoth(e) {
   fetch("/api/speed")
     .then(resSpeed => resSpeed.json())
     .then(resSpeed => {
-      console.log("Speed Cameras array: ", resSpeed); 
-      return resSpeed; // logging step to check what we got
+      console.log("Speed Cameras array: ", resSpeed); // logging step to check what we got
+      return resSpeed;
     })
-    .then(resSpeed => mapSpeedData(resSpeed, false))
+    .then(resSpeed => mapSpeedData(resSpeed, false));
 
-    fetch("/api/redlight")
+  fetch("/api/redlight")
     .then(resRedLight => resRedLight.json())
     .then(resRedLight => {
-      console.log("Red Light Cameras array: ", resRedLight);
-      return resRedLight; // logging step to check what we got
+      console.log("Red Light Cameras array: ", resRedLight); // logging step to check what we got
+      return resRedLight;
     })
     .then(resRedLight => mapRedLightData(resRedLight, false));
 }
@@ -63,18 +69,31 @@ const mapSpeedData = (arg, clearMap) => {
   }
 
   const speedCamIcon = L.icon({
-    iconUrl: '../speed-camera-icon.svg',
+    iconUrl: "../icons/speed-camera-icon.svg",
     iconSize: [40, 80],
-    iconAnchor: [22, 94],
-});
+    iconAnchor: [22, 94]
+  });
 
   /* loop that displays all of the map points as pop ups */
-  for (let i = 0; i < arg.length; i += 1){
-    umd_mark = L.marker([arg[i].latitude, arg[i].longitude], {icon: speedCamIcon}).addTo(mymap);
-    umd_mark.bindPopup("<b>SPEED CAMERA</b></br>" + arg[i].street_address + "</br>" + "<b>Posted speed</b>: " + arg[i].posted_speed + "</br>" + "<b>Enforced speed</b>: " + arg[i].enforcement).openPopup();
+  for (let i = 0; i < arg.length; i += 1) {
+    umd_mark = L.marker([arg[i].latitude, arg[i].longitude], {
+      icon: speedCamIcon
+    }).addTo(mymap);
+    umd_mark
+      .bindPopup(
+        "<b>SPEED CAMERA</b></br>" +
+          arg[i].street_address +
+          "</br>" +
+          "<b>Posted speed</b>: " +
+          arg[i].posted_speed +
+          "</br>" +
+          "<b>Enforced speed</b>: " +
+          arg[i].enforcement
+      )
+      .openPopup();
     umd_mark.addTo(layer1);
-  } 
-}
+  }
+};
 
 const mapRedLightData = (arg, clearMap) => {
   if (clearMap) {
@@ -82,32 +101,34 @@ const mapRedLightData = (arg, clearMap) => {
   }
 
   const redLightCamIcon = L.icon({
-    iconUrl: '../red-light-camera-icon.svg',
+    iconUrl: "../icons/red-light-camera-icon.svg",
     iconSize: [40, 80],
-    iconAnchor: [22, 94],
-});
+    iconAnchor: [22, 94]
+  });
 
-  /* loop that displays all of the map points as pop ups */
-  for (let i = 0; i < arg.length; i += 1){
-    umd_mark = L.marker([arg[i].latitude, arg[i].longitude], {icon: redLightCamIcon}).addTo(mymap);
-    umd_mark.bindPopup("<b>RED LIGHT CAMERA</b></br>" + arg[i].street_address).openPopup();
+  /* loop that displays all of the map points as markers */
+  for (let i = 0; i < arg.length; i += 1) {
+    umd_mark = L.marker([arg[i].latitude, arg[i].longitude], {
+      icon: redLightCamIcon
+    }).addTo(mymap);
+    umd_mark
+      .bindPopup("<b>RED LIGHT CAMERA</b></br>" + arg[i].street_address)
+      .openPopup();
     umd_mark.addTo(layer1);
-  } 
-}
+  }
+};
 
 let selectedInput = "speed";
 
-document.querySelector(".btn").addEventListener("click", (e) => {
-  if(selectedInput === "speed") {
+document.querySelector(".btn").addEventListener("click", e => {
+  if (selectedInput === "speed") {
     loadSpeedCams(e);
-  }
-  else if(selectedInput === "red light") {
+  } else if (selectedInput === "red light") {
     loadRedLightCams(e);
-  }
-  else {
+  } else {
     loadBoth(e);
   }
 });
-document.querySelector(".input-field").addEventListener("change", (e) => {
+document.querySelector(".input-field").addEventListener("change", e => {
   selectedInput = e.target.value;
-})
+});
