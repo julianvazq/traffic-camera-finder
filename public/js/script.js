f26c2984275b8f80d1d3258420b71c3fd558992b
Julian Vazquez <julianvazquez101@gmail.com>
2019-11-29 23:01:09 -0500

    Redo summary section

--- public/js/script.js
+++ public/js/script.js
@@ -38,7 +38,10 @@ function loadSpeedCams(e) {
       console.log("Speed Cameras array: ", res); // logging step to check what we got
       return res;
     })
-    .then(res => mapSpeedData(res));
+    .then(res => {
+      mapSpeedData(res);
+      return res;
+    }).then(res => displaySummary(res));
 }
 
 function loadRedLightCams(e) {
@@ -49,7 +52,10 @@ function loadRedLightCams(e) {
       console.log("Red Light Cameras array: ", res); // logging step to check what we got
       return res;
     })
-    .then(res => mapRedLightData(res));
+    .then(res => {
+      mapRedLightData(res);
+      return res;})
+    .then(res => displaySummary(res));
 }
 
 function loadBoth(e) {
@@ -58,13 +64,20 @@ function loadBoth(e) {
   // We clear the map here to avoid mapRedLightData clearing the speed cam icons
   layer1.clearLayers();
 
+  // Clear display here for same reason as above
+  document.querySelector(".summary").innerHTML = "";
+
   fetch("/api/speed")
     .then(resSpeed => resSpeed.json())
     .then(resSpeed => {
       console.log("Speed Cameras array: ", resSpeed); // logging step to check what we got
       return resSpeed;
     })
-    .then(resSpeed => mapSpeedData(resSpeed, false));
+    .then(resSpeed => {
+      mapSpeedData(resSpeed, false);
+      selectedInput = "speed";
+      return resSpeed;})
+    .then(resSpeed => displaySummary(resSpeed, false));
 
   fetch("/api/redlight")
     .then(resRedLight => resRedLight.json())
@@ -72,7 +85,11 @@ function loadBoth(e) {
       console.log("Red Light Cameras array: ", resRedLight); // logging step to check what we got
       return resRedLight;
     })
-    .then(resRedLight => mapRedLightData(resRedLight, false));
+    .then(resRedLight => {
+      mapRedLightData(resRedLight, false);
+      selectedInput = "red light";
+      return resRedLight;})
+    .then(resRedLight => displaySummary(resRedLight, false));
 }
 
 function loadPoliceStations(e) {
@@ -84,7 +101,10 @@ function loadPoliceStations(e) {
       console.log("Police stations array: ", resPolice); // logging step to check what we got
       return resPolice;
     })
-    .then(resPolice => mapPoliceData(resPolice));
+    .then(resPolice => {
+      mapPoliceData(resPolice);
+      return resPolice;})
+      .then(resPolice => displaySummary(resPolice));
   }
 
 const mapSpeedData = (arg, clearMap = true) => {
@@ -173,23 +193,53 @@ const mapPoliceData = (arg) => {
   }
 };
 
+function displaySummary(objArray, clearSummary = true) {
+
+  const summaryDiv = document.querySelector(".summary");
+
+  if (clearSummary) {
+  summaryDiv.innerHTML = "";
+  }
+
+  let summaryTitle = "";
+  let listItems = [];
+
+  // Display SPEED CAMERA summary
+  if (selectedInput === "speed") {
+    summaryTitle = "SPEED CAMERAS";
+    listItems = 
+      objArray.map(cam => `<li><span>${cam.street_address}</span><div><span>Posted speed: ${cam.posted_speed}</span><span>Enforced speed: ${cam.enforcement}</span></div></li>`)
+  }
+  // Display RED LIGHT CAMERA summary
+  else if (selectedInput === "red light") {
+    summaryTitle = "RED LIGHT CAMERAS";
+    listItems = 
+      objArray.map(cam => `<li><span>${cam.street_address}</span></li>`)
+  }
+
+  else if (selectedInput === "police stations") {
+    summaryTitle = "POLICE STATIONS";
+    listItems = 
+      objArray.map(station => `<li><span>Station name: ${station.name}</span><span>Telephone: ${station.telephone}</span></li>`)
+  }
+  
+  // Update DOM 
+  summaryDiv.innerHTML += `
+  <h2 class="list-title">${summaryTitle}</h2>
+  <ul class="summary-list">${listItems.join("")}</ul>`;
+}
+
 let selectedInput = "speed";
 
 document.querySelector(".btn").addEventListener("click", e => {
   if (selectedInput === "speed") {
     loadSpeedCams(e);
-    document.querySelector(".speed-camera-count").style.display = "flex";
-    document.querySelector(".red-light-count").style.display = "none";
   } else if (selectedInput === "red light") {
     loadRedLightCams(e);
-    document.querySelector(".red-light-count").style.display = "flex";
-    document.querySelector(".speed-camera-count").style.display = "none";
    } else if (selectedInput === "police stations") {
       loadPoliceStations(e);
   } else {
     loadBoth(e);
-    document.querySelector(".red-light-count").style.display = "flex";
-    document.querySelector(".speed-camera-count").style.display = "flex";
   }
 });
 document.querySelector(".input-field").addEventListener("change", e => {
